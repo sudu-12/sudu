a42666adcd89cb03e2af2cd8780614c5dc5df590
sudu-12 <suduladisa@gmail.com>
2020-09-18 05:40:26 +0530

    added csc

---
+++ cscExample.java
@@ -0,0 +1,54 @@
+public class cscExample.java
+{
+	int questionNo();
+	String category();
+	String text();
+	
+	public void setQuestionNo(int questionNo)
+	{
+		this.questionNo=questionNo;
+	}
+	
+	
+	public void setCategory(String category)
+	{
+		this.category=category;
+	}
+	
+	public void setText(String text)
+	{
+		this.text=text;
+	}
+	
+	public int getQuestionNo()
+	{
+		return questionNo;
+	}
+	
+	public String getCategory()
+	{
+		return category;
+	}
+	
+	public String getText()
+	{
+		return text;
+	}
+	
+	public void AddQuestionNo(int questionNo)
+	{
+		this.questionNo=questionNo;
+	}
+
+	public void RemoveQuestionNo(int questionNo)
+	{
+		this.questionNo=questionNo;
+	}
+	
+	public void DeleteQuestionNo(int questionNo)
+	{
+		this.questionNo=questionNo;
+	}
+	
+	
+}
